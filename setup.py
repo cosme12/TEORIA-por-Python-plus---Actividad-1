5c76bda350313b91efc897c2ded8cbc692987f00
Diego Lanciotti <aaadiego502@gmail.com>
2021-04-29 22:38:21 -0300

    Agrega setup para instalar como paquete

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Actividad 1 Teoria',
+    version='1.0',
+    description='Resolucion de la actividad 1 de teoria de Seminario de Leguajes Python',
+    author='Diego Lanciotti',
+    author_email='aaadiego502@gmail.com',
+    url='',
+    packages=['python', 'unlp'],
+)
